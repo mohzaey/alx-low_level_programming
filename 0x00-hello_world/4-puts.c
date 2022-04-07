86585d53959926af3356a3895abdb9a8c037a631
mohzaey <moh.balogun@gmail.com>
2022-04-07 16:12:53 -0700

    hpwdy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,9 @@
  */
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
+	
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return 0;
 
-	puts(Mystr);
 }
