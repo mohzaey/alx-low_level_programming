89ed9e2b47d56d5c26d24a4ff84aae9f1652dbf3
mohzaey <moh.balogun@gmail.com>
2022-04-12 19:53:32 -0700

    howdy

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	print_times_table(2);
-	return (0);
+    print_times_table(2);
+    _putchar('\n');
+    print_times_table(10);
+    _putchar('\n');
+    print_times_table(13);
+    _putchar('\n');
+    print_times_table(-2);
+    _putchar('\n');
+    print_times_table(0);
+    _putchar('\n');
+    print_times_table(100);  
+    return (0);
 }
